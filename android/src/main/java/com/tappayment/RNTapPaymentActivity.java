4739b9d2d4bbb783eeacf434ce6f2cceac0becc9
peymancyb <paymanghazvini@yahoo.com>
2019-08-19 15:26:36 +0400

    getCustomer method

--- android/src/main/java/com/tappayment/RNTapPaymentActivity.java
+++ android/src/main/java/com/tappayment/RNTapPaymentActivity.java
@@ -99,9 +99,10 @@ public class RNTapPaymentActivity extends AppCompatActivity implements SessionDe
 
     private Customer getCustomer() {
         Bundle extras = getIntent().getExtras();
-        String CustomerIdString = extras.getString("CustomerId");
-        String CustomerId = (CustomerIdString.length() == 0) ? null : CustomerIdString;
-        return new Customer.CustomerBuilder(CustomerId).build();
+        String CustomerId = extras.getString("CustomerId");
+        return new Customer.CustomerBuilder(CustomerId).email("abc@abc.com").firstName("firstname")
+            .lastName("lastname").metadata("").phone(new PhoneNumber("965","65562630"))
+            .middleName("middlename").build();
     }
 
     public void paymentSucceed(@NonNull Charge charge) {
